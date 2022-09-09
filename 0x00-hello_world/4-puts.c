5e0f24c93d4d82272882b0288d3ed8354f0214e6
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-08 22:45:27 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  * main- entry point
+ *   *
+ *    * Return: ALways returns 0 for smooth execution
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
