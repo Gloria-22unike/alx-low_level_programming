74e3b192058104fed22641ff4e70a7c1ce5d9ce8
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-11 13:53:26 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,,
+ * followed by a new line
+ * Retuen: returns 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch==)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
