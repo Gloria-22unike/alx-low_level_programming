66c2379e848a190611e7e62c47d98e890f7679be
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-14 18:45:23 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -16,9 +16,9 @@ int main(void)
 
 	for (start_num = 0; start_num < end_num; start_num++)
 	{
-		if ((start_num % 3 == 0) || (start_num % 5 == 0)
-		(
-		 	total = total + start_num;
+		if ((start_num % 3 == 0) || (start_num % 5 == 0))
+		{
+			total = total + start_num;
 		}
 		else
 		{
@@ -29,6 +29,7 @@ int main(void)
 	printf("%d", total);
 	printf("\n");
 
-	return (0);
+	return(0);
 }
 
+	
