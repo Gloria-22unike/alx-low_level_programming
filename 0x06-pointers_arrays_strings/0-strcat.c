5e016a40c43cc5e27cff2796cdf7e7e90be40251
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-22 15:16:58 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int count = 0;
+	int count2 = 0;
 
-	while (*(dest + count) != '\0')
+	while (*(dest + count) !=  '\0')
 	{
 		count++;
 	}
-	while (count2 >= 0)
+	while (count >= 0)
 	{
-		*(dest + count) = *(src + count2);
+	       	*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
 		count++;
