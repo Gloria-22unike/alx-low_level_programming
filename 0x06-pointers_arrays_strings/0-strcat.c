4cd8ddb1eff17a35b221e3e1dd443155d10790bb
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-22 15:58:03 -0700

    join 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
- * _strcat _concatenates two strings,
- * @dest: destination,
- * @src: source.
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source
  * Return: the pointer to dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int count = 0;
-	int count2 = 0;
+	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
-	{
 		count++;
-	}
 	while (count2 >= 0)
 	{
-	       	*(dest + count) = *(src + count2);
+		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
 		count++;
