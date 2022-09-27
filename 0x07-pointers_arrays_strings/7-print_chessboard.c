44f740a76f137a65cfc1de9bc25caaf14399a52f
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-27 03:10:15 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: two dimension array to print
+ * @a: matrix
  */
 void print_chessboard(char (*a)[8])
 {
@@ -12,7 +12,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(*(i +a) + j));
 		}
 		_putchar('\n');
 	}
