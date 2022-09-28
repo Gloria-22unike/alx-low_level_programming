a3a7272bd8d14d23424ad41c331ff5cc7665e1ef
Gloria-22unike <gloria_dotse@yahoo.com>
2022-09-28 12:58:04 -0700

    in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: input string
+ *
+ * Return: On success 1.
+ * On errorx, _1 is returned;
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
